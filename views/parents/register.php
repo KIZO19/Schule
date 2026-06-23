7a5023fa0f3fcb01982e64b47e8ba510c4305345
Joseph Kizo <johkizo19@gmail.com>
2026-06-23 23:47:24 +0200

    Auto-commit du 23/06/2026 à 23:47:24,30

--- views/parents/register.php
+++ views/parents/register.php
@@ -8,11 +8,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-p1CmYCoPokrB35YqjKM8KJ6xTg+vVKp5JEcT0BSG0sP3VK8jPQ4UMYUDi+AHGp/SxRzPaw8x5Zk6fZx0gd6HAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= BASE_URL ?>/public/assets/css/adminlte.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/public/assets/css/custom.css">
 </head>
-<body class="hold-transition login-page">
+<body class="hold-transition login-page login-page-brand">
 <div class="login-box">
-    <div class="login-logo">
-        <a href="<?= BASE_URL ?>/"><b>School</b>Gest</a>
+    <div class="login-logo text-center">
+        <a href="<?= BASE_URL ?>/" class="text-white"><span class="login-brand-title">SMSys</span><br><small class="text-light">Inscription parent</small></a>
     </div>
     <div class="card">
         <div class="card-body login-card-body">
@@ -94,9 +95,14 @@
                         <div class="input-group-text"><span class="fas fa-lock"></span></div>
                     </div>
                 </div>
+                <div class="row mt-4">
+                    <div class="col-12 text-center mb-3">
+                        <small class="text-muted">Entrez les informations ci-dessus pour créer un compte parent sécurisé.</small>
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-8">
-                        <a href="<?= BASE_URL ?>/">J'ai déjà un compte</a>
+                        <a href="<?= BASE_URL ?>/" class="text-primary font-weight-bold">J'ai déjà un compte</a>
                     </div>
                     <div class="col-4">
                         <button type="submit" class="btn btn-primary btn-block">S'inscrire</button>
